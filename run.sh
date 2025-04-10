1b32b27be4acf365b9b2c844ce8b1ee7e159ffb7
Rodolfo Azevedo <rodolfo.azevedo@unicamp.br>
2025-04-10 16:37:39 -0300

    atualizando o teste00 para ter apenas uma sequência pequena de instruções. leia o arquivo .mem para entender. Além disso, mudando o tb.v para imprimir apenas as escritas em memória.

--- run.sh
+++ run.sh
@@ -9,15 +9,12 @@ if ! iverilog -o tb *.v; then
 fi
 
 cp test/teste$1.mem memory.mem
-./tb > saida.tmp
-grep '===' saida.tmp > saida.out
-rm saida.tmp
+./tb | grep '===' > saida.tmp
 cp saida.out test/saida$1.out
 cp saida.vcd test/saida$1.vcd
 rm saida.out saida.vcd memory.mem
 
-if diff test/saida$1.out test/saida$1.ok >/dev/null; then
-    echo "OK"
+if diff --strip-trailing-cr test/saida$1.out test/saida$1.ok >/dev/null; then    echo "OK"
     exit 0
 else
     echo "ERRO"
--- tb.v
+++ tb.v
@@ -43,8 +43,8 @@ always @(posedge clk) begin
   else if (address[11] == 1)
     if (we == 1)
       $display("=== M[0x%h] <- 0x%h", address, data_out);
-    else
-      $display("=== M[0x%h] -> 0x%h", address, data_in);
+    // else
+    //   $display("=== M[0x%h] -> 0x%h", address, data_in);
 end
 
 endmodule
